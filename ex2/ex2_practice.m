eedbde4cee7f600d62ed87d11a5e7a4d7d27ec00
Haebuk <cwryu6252@naver.com>
2021-01-06 20:39:56 +0900

    Update ex2

--- ex2/ex2_practice.m
+++ ex2/ex2_practice.m
@@ -1,4 +1,4 @@
-% Load Data
+%% Load Data
 % The first two columns contain the exam scores and the third column contains the label.
 data = load('ex2data1.txt');
 X = data(:, [1, 2]); 
@@ -17,7 +17,7 @@ y = data(:, 3);
 % % Specified in plot order
 % legend('Admitted', 'Not admitted')
 
-%%
+%% Setup Logistic Regression Cost & Gradient
 
 %  Setup the data matrix appropriately
 [m, n] = size(X);
@@ -28,7 +28,8 @@ X = [ones(m, 1) X];
 % Initialize the fitting parameters
 initial_theta = zeros(n + 1, 1);
 
-%% 
+%% Test Logistic Regression Cost & Gradient
+
 % Compute and display the initial cost and gradient
 [cost, grad] = costFunction(initial_theta, X, y);
 fprintf('Cost at initial theta (zeros): %f\n', cost);
@@ -40,6 +41,37 @@ test_theta = [-24; 0.2; 0.2];
 [cost, grad] = costFunction(test_theta, X, y);
 fprintf('\nCost at non-zero test theta: %f\n', cost);
 disp('Gradient at non-zero theta:'); disp(grad);
+
+%% Predict
+
+%  Set options for fminunc
+options = optimoptions('fmincon','Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 400);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+disp('theta:');disp(theta);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+% Add some labels 
+hold on;
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+hold off;
+
+
+
+
+
+
+
 
 
 
