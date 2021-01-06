6525a7d822d38e255f89723c76a44dfa8a8de18d
Haebuk <cwryu6252@naver.com>
2021-01-06 20:23:04 +0900

    Update ex2

--- ex2/ex2_practice.m
+++ ex2/ex2_practice.m
@@ -3,16 +3,44 @@
 data = load('ex2data1.txt');
 X = data(:, [1, 2]); 
 y = data(:, 3);
-% Find Indices of Positive and Negative Examples
-pos = find(y==1); neg = find(y == 0);
-% Plot Examples
-plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7); 
-hold on;
-plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
-hold off;
-% Labels and Legend
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
+% % Find Indices of Positive and Negative Examples
+% pos = find(y==1); neg = find(y == 0);
+% % Plot Examples
+% plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7); 
+% hold on;
+% plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
+% hold off;
+% % Labels and Legend
+% xlabel('Exam 1 score')
+% ylabel('Exam 2 score')
+% 
+% % Specified in plot order
+% legend('Admitted', 'Not admitted')
 
-% Specified in plot order
-legend('Admitted', 'Not admitted')
+%%
+
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+%% 
+% Compute and display the initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+disp('Gradient at initial theta (zeros):'); 
+disp(grad);
+
+% Compute and display cost and gradient with non-zero theta
+test_theta = [-24; 0.2; 0.2];
+[cost, grad] = costFunction(test_theta, X, y);
+fprintf('\nCost at non-zero test theta: %f\n', cost);
+disp('Gradient at non-zero theta:'); disp(grad);
+
+
+
+
